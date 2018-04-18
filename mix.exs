af120208b9ab612a7602b8a605584717e2d25c9e
KobaruTakahiro <kobaru@fusic.co.jp>
2018-04-18 19:23:53 +0900

    release 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ElixirLdap.Mixfile do
   def project do
     [
       app: :elixir_ldap,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       description: "elixir LDAP Client",
